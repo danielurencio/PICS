90a43ec8ebd387151b60facb1971f9b647da1308
Daniel <danielurencio@gmial.com>
2016-09-16 18:34:54 -0500

    modified script.sh

--- data/script.sh
+++ data/script.sh
@@ -52,38 +52,33 @@ function cadaEntidad {
 }
 
 # Con esto se ordenan cada tipo de cartografía en una carpeta diferente.
-function ordenar {
+ordenar() {
   cd entidades; #mkdir municipios;
 
   for i in *; do
-   if [ -f $i ]; then
+
       filename=${i%.*};
       mkdir $filename;
       unzip $i -d $filename;
-      cd $filename/conjunto_de_datos;
 
       # Esto crea una carpeta para tipo de cartografía.
-	for f in *; do
-	  var="${f%.*}";
+	for f in ${filename}/conjunto_de_datos/*; do
+	  name=$(basename $f);
+	  var="${name%.*}";
 	  length=${#var}
 	  dir=${var:2:$length};
 
         # Sin no existe la carpeta, entonces se crea.
-	  if [ ! -d "${dir}" ]; then
-	    mkdir ${dir}
+	  if [ ! -d ${filename}/conjunto_de_datos/${dir} ]; then
+	    mkdir ${filename}/conjunto_de_datos/${dir}
 	  fi
-	# Esto mueve los archivos a cada carpeta. Los '??' es para el # de entidad
-	  if [[ ??${dir}.* ]]; then
-	    mv ??${dir}.* ${dir}
+
+	  if [[ ${filename}/conjunto_de_datos/??${dir}.* ]]; then
+	    mv ${filename}/conjunto_de_datos/??${dir}.* ${filename}/conjunto_de_datos/${dir}
 	  fi
+
 	done
 
-    #ogr2ogr -t_srs EPSG:4326 -f GeoJSON ${filename}_MUN.json *mun.shp
-    #topojson -o ${filename}_mun.json ${filename}_MUN.json -p
-    #mv ${filename}_mun.json ../../
-      cd ../..;
-      #mv *.json municipios;
-   fi
   done
 
 #  cd municipios; mkdir exports; cd ../../;
@@ -105,56 +100,10 @@ function transformar {
     for d in */; do
       ogr2ogr -t_srs EPSG:4326 -f GeoJSON \
 ${c}/${d::-1}_${c}.json ${d}conjunto_de_datos/${c}/*.shp;
+      topojson -o ${c}/${d::-1}_${c}_t.json ${c}/${d::-1}_${c}.json;
     done
   done
 
   echo "Finito.."
 }
 
-
-## DENUE ##################|
-function denueDescarga {
- links=(
-  Aguascalientes
-  Baja_California
-  Baja_California_Sur
-  Campeche
-  Coahuila_de_Zaragoza
-  Colima
-  Chiapas
-  Chihuahua
-  Distrito_Federal
-  Durango
-  Guanajuato
-  Guerrero
-  Hidalgo
-  Jalisco
-  Mexico
-  Michoacan_de_Ocampo
-  Morelos
-  Nayarit
-  Nuevo_Leon
-  Oaxaca
-  Puebla
-  Queretaro
-  Quintana_Roo
-  San_Luis_Potosi
-  Sinaloa
-  Sonora
-  Tabasco
-  Tamaulipas
-  Tlaxcala
-  Veracruz_de_Ignacio_de_la_Llave
-  Yucatan
-  Zacatecas)
-
- mkdir denue
-
- for i in ${!links[@]}; do
-   num=`expr $i + 1`;
-   if [ ${#num} == 1 ]; then num=0$num; fi
-#  echo $num ${links[$i]}
-   curl -o ${num}.zip http://www3.inegi.org.mx/sistemas/descarga/descargaarchivo.aspx?file=DENUE%2fEntidad_federativa%2f${num}_${links[$i]}%2fdenue_${num}_shp.zip
- done
-
-}
